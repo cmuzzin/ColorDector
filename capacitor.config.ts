ef69b3d2fa9b5e63d70853470ac64f119f163c27
Chris Muzzin <chrismuzzin@yahoo.com>
2024-05-28 12:09:32 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'color-detector',
+  webDir: 'www'
+};
+
+export default config;
